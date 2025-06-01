fa77784f4125a7ae810e4d441f8e0781fc84fd2d
4zyGy <4zygy.64@gmail.com>
2025-06-01 03:15:38 +0100

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,14 @@
+const songs = [
+  {
+    title: "รักแรกพบ",
+    slug: "rak-raek-phop",
+    lyrics: "รักแรกพบ รักครั้งเดียวในชีวิต...",
+    youtube: "https://www.youtube.com/embed/XXXXXXXX"
+  },
+  {
+    title: "ยังคงเป็นเธอ",
+    slug: "yang-khong-pen-ter",
+    lyrics: "เธอยังคงเป็นคนที่ฉันรัก...",
+    youtube: "https://www.youtube.com/embed/YYYYYYYY"
+  }
+];
